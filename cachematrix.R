0d5938d3ee65f3986cfd927a56cd80c036c3d2a1
n8atkinson <n8atkinson@users.noreply.github.com>
2017-07-31 09:28:05 -0600

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This a is secondary function call that returns already calculated matrix inverse or that it needs to be done
 
 makeCacheMatrix <- function(x = matrix()) {
+        inverse <- NULL
+        
+        # Still a little unclear about this
+        set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+        }
+        
+        # 
+        get <- function() x
+        
+        # 
+        set_inverse <- function(matrix_inverse) inverse <<- matrix_inverse
+        
+        #
+        get_inverse <- function() inverse
+        
+        # Returns a list of functions allowing them to exist in parent environment
+        list(set = set, get = get, get_inverse = get_inverse, set_inverse = set_inverse)
 
 }
 
 
-## Write a short comment describing this function
+## This is the primary function call of the original matrix that will have its inverse calculated
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # Pull in either NULL or already calculated inverse
+        inverse <- x$get_inverse
+        
+        # If inverse<>NULL then return already calcuated inverse
+        # Question: where is the check if inverse changes instead of being NULL?
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+        
+        # This is like an else_if,  if previous "return" is called then cacheSolve closes at that line
+        
+        # The rest of cacheSolve runs if inverse=NULL, meaning inverse needs to be calculated
+        data <- x$get_inverse()
+        
+        # 
+        inverse <- solve(data, ...)
+        
+        #
+        x$set_inverse(inverse)
+        
+        # Return new calculated inverse
+        inverse
 }
+
+
+# Ref: https://github.com/lgreski/datasciencectacontent/blob/master/markdown/rprog-breakingDownMakeVector.md
